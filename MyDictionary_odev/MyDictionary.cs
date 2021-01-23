39d8ade4c9b8d0915d52aca9f089800384262e54
özkan göktaş <karozkan@yahoo.com>
2021-01-23 20:24:02 +0300

    Add project files.

---
+++ MyDictionary_odev/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary_odev
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+            keys = new K[tempKeys.Length + 1];
+            values = new V[tempValues.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[tempKeys.Length] = key;
+            values[tempValues.Length] = value;
+        }
+        public void Prints()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(values[i]+":"+keys[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary_odev/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, byte> myDictionary = new MyDictionary<string,byte>();
+            myDictionary.Add("Ankara", 06);
+            myDictionary.Add("Karabük", 78);
+            myDictionary.Add("Hatay", 31);
+
+            myDictionary.Prints();
+        }
+    }
+
+}
